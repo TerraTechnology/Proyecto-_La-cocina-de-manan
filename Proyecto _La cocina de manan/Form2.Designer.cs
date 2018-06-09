736e1f1948b8316f47418a26df7ec23059b7f726
Estudiante <Estudiante42@outlook.com>
2018-06-09 02:08:45 -0400

    hola

--- Proyecto _La cocina de manan/Form2.Designer.cs
+++ Proyecto _La cocina de manan/Form2.Designer.cs
@@ -68,7 +68,7 @@
 			// comboBox1
 			// 
 			this.comboBox1.FormattingEnabled = true;
-			this.comboBox1.Location = new System.Drawing.Point(319, 94);
+			this.comboBox1.Location = new System.Drawing.Point(336, 62);
 			this.comboBox1.Name = "comboBox1";
 			this.comboBox1.Size = new System.Drawing.Size(121, 24);
 			this.comboBox1.TabIndex = 3;
