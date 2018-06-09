29db058df24972f66ddd3ca531e74043a50f114d
kitomc <kitomc.rd@gmail.com>
2018-06-08 23:02:04 -0400

    otro cambio de no se que

--- Proyecto _La cocina de manan/Form2.Designer.cs
+++ Proyecto _La cocina de manan/Form2.Designer.cs
@@ -46,7 +46,7 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(490, 381);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(112, 20);
             this.textBox1.TabIndex = 1;
@@ -55,7 +55,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(625, 450);
+            this.ClientSize = new System.Drawing.Size(635, 450);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
